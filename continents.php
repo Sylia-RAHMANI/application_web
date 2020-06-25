578f3d8d02bb3ef32a11fabb0ecab1a4651af11a
Sylia RAHMANI <sy.rahmani@yahoo.fr>
2020-06-25 19:43:45 +0200

    first commit

---
+++ continents.php
@@ -0,0 +1,168 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>C&F Game</title>
+        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <!--<script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>-->
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+        <script
+        src="https://kit.fontawesome.com/83f4286022.js"
+        crossorigin="anonymous"
+      ></script>
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand js-scroll-trigger" href="#page-top"> <h2 class="vp-logo">
+                    <i class="fas fa-globe-americas"></i><a href="#"> C&F Game </a></h2></a><button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">Menu<i class="fas fa-bars ml-1"></i></button>
+                   <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav text-uppercase ml-auto">
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.html"><i class="fa fa-home" aria-hidden="true"></i></a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="./vue/inscription.php"><i class="fas fa-cash-register"></i></a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="./controle/identification.php"><i class="fas fa-sign-in-alt"></i> </a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="./vue/aide.php"><i class="fa fa-file-text-o" aria-hidden="true"></i></a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#"> <i class="fa fa-trophy" aria-hidden="true"></i></a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#team"><i class="fa fa-unlock-alt" aria-hidden="true"></i></a></li>
+                        
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container">
+            
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase"> Liste des continents </h2>
+                    <h3 class="section-subheading text-muted"> Choisissez votre continent préfèrer.</h3>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 col-sm-6 mb-4">
+                        <div class="portfolio-item">
+                            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1"
+                                ><div class="portfolio-hover">
+                                    <div class="portfolio-hover-content"></div>
+                                </div>
+                                <button type="button" class="btn btn-outline-light btn-lg"> <a href="jeu/ameriqueNord.php"  ><img class="img-fluid" src="assets/img/contientAm.webp" alt=""
+                            /></a>
+                            <div class="portfolio-caption">
+                                <div class="portfolio-caption-heading"> Amérique </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-6 mb-4">
+                        <div class="portfolio-item">
+                            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal2"
+                                ><div class="portfolio-hover">
+                                    <div class="portfolio-hover-content"></div>
+                                </div>
+                                <button type="button" class="btn btn-outline-light btn-lg"> <a href="jeu/asie.php"  ><img class="img-fluid" src="assets/img/contientAs.webp" alt=""
+                            /></a>
+                            <div class="portfolio-caption">
+                                <div class="portfolio-caption-heading"> Asie </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-6 mb-4">
+                        <div class="portfolio-item">
+                            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal3"
+                                ><div class="portfolio-hover">
+                                    <div class="portfolio-hover-content"></div>
+                                </div>
+                                <button type="button" class="btn btn-outline-light btn-lg"> <a href="jeu/afrique.php"  ><img class="img-fluid" src="assets/img/contientAf.webp" alt=""
+                            /></a>
+                            <div class="portfolio-caption">
+                                <div class="portfolio-caption-heading"> Afrique </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-6 mb-4 mb-lg-0">
+                        <div class="portfolio-item">
+                            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal4"
+                                ><div class="portfolio-hover">
+                                    <div class="portfolio-hover-content"></div>
+                                </div>
+                                <button type="button" class="btn btn-outline-light btn-lg"> <a href="jeu/Oceanie.php"  ><img class="img-fluid" src="assets/img/contientAf.webp" alt=""
+                            /></a>
+                            <div class="portfolio-caption">
+                                <div class="portfolio-caption-heading">Océanie</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-6 mb-4 mb-sm-0">
+                        <div class="portfolio-item">
+                            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal5"
+                                ><div class="portfolio-hover">
+                                    <div class="portfolio-hover-content"></div>
+                                </div>
+                                <button type="button" class="btn btn-outline-light btn-lg"> <a href="jeu/europe.php"  ><img class="img-fluid" src="assets/img/contientER.webp" alt=""
+                            /></a>
+                            <div class="portfolio-caption">
+                                <div class="portfolio-caption-heading">Europe</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-6 mb-4 mb-sm-0">
+                        <div class="portfolio-item">
+                            <a class="portfolio-link" data-toggle="modal" href=""
+                                ><div class="portfolio-hover">
+                                    <div class="portfolio-hover-content"></div>
+                                </div>
+                                <button type="button" class="btn btn-outline-light btn-lg"> <a href="jeu/jeu.php"  ><img class="img-fluid" src="assets/img/continentWr.jpg" alt=""
+                            /></a>
+                                </a>
+                            <div class="portfolio-caption">
+                                <div class="portfolio-caption-heading">Planète terre</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+       
+            </div>
+        </header>
+      
+        <!-- Carousel-->
+        
+                
+        <!--Carousel Wrapper-->
+        
+       
+       
+        <!-- Footer-->
+        <footer class="footer py-4">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-4 text-lg-left"> Web project 2020</div>
+                    <div class="col-lg-4 my-3 my-lg-0">
+                    </div>
+                    <div class="col-lg-4 text-lg-right"><a href="#!">C&F Game</a></div>
+                </div>
+            </div>
+        </footer>
+        
+        <!-- Bootstrap core JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
+        <!-- Third party plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+        <!-- Contact form JS-->
+        <script src="assets/mail/jqBootstrapValidation.js"></script>
+        <script src="assets/mail/contact_me.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
