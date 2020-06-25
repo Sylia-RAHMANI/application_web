c0558e73e004969b8411230825feb951a1540ba9
Sylia RAHMANI <sy.rahmani@yahoo.fr>
2020-06-25 19:41:13 +0200

    first commit

---
+++ aide.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/942be695f4.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Comic+Neue:wght@400;700&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="./css/styles.css">
+    <title> C&F | Aide </title>
+</head>
+<body>
+<header class="masthead">
+    
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase">Consignes du jeu</h2> 
+                    <br>
+                    <br>
+                </div>
+                <ul class="timeline">
+                    <li>
+                        <div class="timeline-image text-center"> </div>
+                        <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                <h4 class="subheading text-center">Règles du jeu</h4>
+                            </div>
+                            <div class="timeline-body"><p class="text-center">Un questionnaire est constitué d'un certain nombre de questions, ce nombre dépend du mode avec lequel le joueur joue. Le but du jeu est de trouver à chauqe fois sur la carte le pays dont le drapeau est donné en question ceci avec le plus de score possible</p></div>
+                        </div>
+                    </li>
+                    <li class="timeline-inverted">
+                        <div class="timeline-image"></div>
+                        <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                <h4 class="subheading text-center">Le Mode Invité</h4>
+                            </div>
+                            <div class="timeline-body"><p class="text-center">Le mode inivité ne nécessite pas de connexion, il est constitué de 5 questions. Avec ce mode le joueur a seulement le droit de rejouer au meme questionnaire</p></div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="timeline-image"></div>
+                        <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                <h4 class="subheading text-center">Le Mode Connecté</h4>
+                            </div>
+                            <div class="timeline-body"><p class="text-center">Le mode connecté nécessite une connexion, pour cela le joueur doit s'inscrire et ensuite se connecter. Ce mode offre une palette de choix au joueur. Une fois connecté le joueur a la possibilité de sélectionner un continent sur lequel jouer parmi les 5 continents possibles: Europe, Afrique, Amérique, Asie, Océanie. Son questionnaire est composé de 10 questions dans ce cas là.
+Une version plus développée et plus difficle est possible aussi, en sélectionnant le mode planétaire le joueur joue sur toute la carte du mode avec 15 questions</p></div>
+                        </div>
+                    </li>
+                    <li class="timeline-inverted">
+                        <div class="timeline-image"></div>
+                        <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                <h4 class="subheading text-center">Le Score</h4>
+                            </div>
+                            <div class="timeline-body"><p class="text-center">Le score est attribué en fonction de la surface du pays. Généralement les pays les plus grands sont les plus connus, donc on leur attribut moins de points
+                        </div>
+                    </li>
+                    <li class="timeline-inverted">
+                        <div class="timeline-image">
+                            <h4>Tu attends<br />quoi pour<br />commencer !</h4>
+                        </div>
+                    </li>
+                </ul>
+            
+
+</body>
+</html>
